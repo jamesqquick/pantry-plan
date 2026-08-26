52ca795e952391242108f8dc8b11deef7b796bdc
James Quick <jquick@cloudflare.com>
2026-08-26 08:59:21 -0400

    fix(db): remove duplicate account migration

--- src/db/migrations/0001_account_issuer.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `account` ADD `issuer` text NOT NULL DEFAULT 'local:credential';
---> statement-breakpoint
-CREATE UNIQUE INDEX `account_issuer_account_id_key` ON `account` (`issuer`,`accountId`);
